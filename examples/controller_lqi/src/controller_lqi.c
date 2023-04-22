bee7e1ab2e830e2f05e4db969249780a189ba6a4
xkhainguyen <nxk120199@gmail.com>
2023-04-22 14:16:49 -0400

    add lqi

--- examples/controller_lqi/src/controller_lqi.c
+++ examples/controller_lqi/src/controller_lqi.c
@@ -22,8 +22,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  *
- * controller_lqr.c - App layer application of a quaternion-based LQR.
- * This is one of the best existing LQR controllers you can find online
+ * controller_lqi.c - App layer application of a quaternion-based LQI (Linear Quadratic Integrator).
+ * This is one of the best existing LQI controllers you can find online
  * for Crazyflie.
  */
 
@@ -59,20 +59,20 @@ void appMain() {
   }
 }
 
-#define NX  13  // no. state variable s       [position (3), attitude (4), body velocity (3), angular rate (3)]
-#define NXt 12  // no. state error variables  [position (3), attitude (3), body velocity (3), angular rate (3)]
+#define NX  13  // no. state variables        [position (3), attitude (4), body velocity (3), angular rate (3)]
+#define NXt 15  // no. augmented error        [position (3), attitude (3), body velocity (3), angular rate (3), integral (3)]
 #define NU  4   // no. control input          [thrust, torque_x, torque_y, torque_z]
-#define LQR_RATE RATE_50_HZ  // control frequency
+#define LQR_RATE RATE_500_HZ  // control frequency
 
 static float K[NU][NXt] = {
-  {0.000000f,0.000000f,0.763388f,0.000000f,0.000000f,0.000000f,0.000000f,0.000000f,0.789927f,0.000000f,0.000000f,0.000000f},
-  {0.000037f,-0.000780f,0.000000f,0.009752f,0.000469f,0.000064f,0.000057f,-0.001177f,0.000000f,0.000876f,0.000042f,0.000015f},
-  {0.000777f,-0.000037f,0.000000f,0.000469f,0.009716f,0.000161f,0.001172f,-0.000057f,0.000000f,0.000042f,0.000873f,0.000036f},
-  {0.000028f,-0.000011f,0.000000f,0.000149f,0.000374f,0.002703f,0.000043f,-0.000017f,0.000000f,0.000015f,0.000037f,0.000610f},
+  {0.000000f,0.000000f,0.934797f,0.000000f,0.000000f,0.000000f,0.000000f,0.000000f,0.516505f,0.000000f,0.000000f,0.000000f,0.000000f,0.000000f,0.000000f},
+  {0.000073f,-0.001549f,0.000000f,0.010829f,0.000511f,0.000035f,0.000071f,-0.001518f,0.000000f,0.000877f,0.000042f,0.000009f,0.000000f,0.000000f,0.000000f},
+  {0.001541f,-0.000073f,0.000000f,0.000511f,0.010780f,0.000088f,0.001510f,-0.000071f,0.000000f,0.000042f,0.000873f,0.000022f,0.000000f,0.000000f,0.000000f},
+  {0.000024f,-0.000010f,0.000000f,0.000083f,0.000211f,0.001522f,0.000026f,-0.000010f,0.000000f,0.000009f,0.000022f,0.000371f,0.000000f,0.000000f,0.000000f},
 };
 
-static float x_error[NXt];  
-static float control_input[NU];
+static float x_error[NXt] = {0};  
+static float control_input[NU] = {0};
 
 // Struct for logging position information
 static bool isInit = false;
@@ -80,6 +80,10 @@ static bool isInit = false;
 void controllerOutOfTreeInit(void) {
   if (isInit) {
     return;
+  }
+
+  for (int i = 0; i < NXt; ++i) {
+    x_error[i] = 0.0f;
   }
 
   isInit = true;
@@ -96,19 +100,19 @@ void controllerOutOfTree(control_t *control, const setpoint_t *setpoint, const s
     return;
   }
   // Positon error, [m]
-  x_error[0] = state->position.x - 0*setpoint->position.x;
-  x_error[1] = state->position.y - 0*setpoint->position.y;
-  x_error[2] = state->position.z - setpoint->position.z;
+  x_error[0] = state->position.x - 1*setpoint->position.x;
+  x_error[1] = state->position.y - 1*setpoint->position.y;
+  x_error[2] = state->position.z - 1*setpoint->position.z;
 
   // Body velocity error, [m/s]                          
-  x_error[6] = state->velocity.x - 0*setpoint->velocity.x;
-  x_error[7] = state->velocity.y - 0*setpoint->velocity.y;
-  x_error[8] = state->velocity.z - 0*setpoint->velocity.z;
+  x_error[6] = state->velocity.x - 1*setpoint->velocity.x;
+  x_error[7] = state->velocity.y - 1*setpoint->velocity.y;
+  x_error[8] = state->velocity.z - 1*setpoint->velocity.z;
 
   // Angular rate error, [rad/s]
-  x_error[9]  = radians(sensors->gyro.x - 0*setpoint->attitudeRate.roll);   
-  x_error[10] = radians(sensors->gyro.y - 0*setpoint->attitudeRate.pitch);
-  x_error[11] = radians(sensors->gyro.z - 0*setpoint->attitudeRate.yaw);
+  x_error[9]  = radians(sensors->gyro.x - 1*setpoint->attitudeRate.roll);   
+  x_error[10] = radians(sensors->gyro.y - 1*setpoint->attitudeRate.pitch);
+  x_error[11] = radians(sensors->gyro.z - 1*setpoint->attitudeRate.yaw);
 
   struct quat attitude_g = qeye();  // goal attitude
   // struct quat attitude_g = mkquat(setpoint->attitudeQuaternion.x, setpoint->attitudeQuaternion.y, setpoint->attitudeQuaternion.z, setpoint->attitudeQuaternion.w);
@@ -127,6 +131,10 @@ void controllerOutOfTree(control_t *control, const setpoint_t *setpoint, const s
   x_error[3] = phi.x;
   x_error[4] = phi.y;
   x_error[5] = phi.z;
+
+  x_error[12] += x_error[0] / LQR_RATE;
+  x_error[13] += x_error[1] / LQR_RATE;
+  x_error[14] += x_error[2] / LQR_RATE;
 
   // Matrix multiplication, compute control input
   for (int i = 0; i < STABILIZER_NR_OF_MOTORS; i++){
