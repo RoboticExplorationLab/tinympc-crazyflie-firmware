99848b992bdc5c525b26105e55a10d623d4972b2
xkhainguyen <nxk120199@gmail.com>
2023-05-18 16:19:54 -0400

    up

---
+++ examples/controller_tinympc/src/controller_tinympc.c
@@ -0,0 +1,279 @@
+/**
+ * ,---------,       ____  _ __
+ * |  ,-^-,  |      / __ )(_) /_______________ _____  ___
+ * | (  O  ) |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+ * | / ,--´  |    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+ *    +------`   /_____/_/\__/\___/_/   \__,_/ /___/\___/
+ *
+ * Crazyflie control firmware
+ *
+ * Copyright (C) 2019 Bitcraze AB
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, in version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * controller_tinympc.c - App layer application of TinyMPC.
+ */
+
+
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "app.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "controller.h"
+#include "physicalConstants.h"
+#include "log.h"
+#include "param.h"
+#include "num.h"
+#include "math3d.h"
+// #include "TinyMPC-ADMM/src/tinympc/tinympc.h"
+
+// Edit the debug name to get nice debug prints
+#define DEBUG_MODULE "CONTROLLER_TINYMPC"
+#include "debug.h"
+
+void appMain() {
+  DEBUG_PRINT("Waiting for activation ...\n");
+
+  while(1) {
+    vTaskDelay(M2T(2000));
+
+    // Remove the DEBUG_PRINT.
+    // DEBUG_PRINT("Hello World!\n");
+  }
+}
+
+#define NX  13  // no. state variables        [position (3), attitude (4), body velocity (3), angular rate (3)]
+#define NXt 13  // no. augmented error        [position (3), attitude (3), body velocity (3), angular rate (3), integral (3)]
+#define NU  4   // no. control input          [thrust, torque_x, torque_y, torque_z] scaled by UINT16_MAX
+#define LQR_RATE RATE_50_HZ  // control frequency
+
+// static float K[NU][NXt] = {
+//   {0.000005f,-0.000140f,52653.331642f,0.000451f,0.000116f,0.000004f,0.000011f,-0.000092f,29655.145947f,0.000002f,0.000001f,0.000000f,25000.0f},
+//   {4.994163f,-102.003187f,-0.000009f,664.757240f,32.583295f,3.411040f,4.777267f,-97.535743f,-0.000001f,57.508688f,2.822074f,1.752151f,0.0f},
+//   {101.840667f,-4.996583f,-0.000122f,32.599051f,663.753591f,8.635494f,97.383191f,-4.779672f,-0.000007f,2.823372f,57.426884f,4.434289f,0.0f},
+//   {8.926535f,-3.567274f,-0.000010f,23.538639f,58.898015f,157.340335f,8.573598f,-3.426313f,-0.000001f,2.062858f,5.161323f,80.567476f,0.0f},
+// };
+
+static float K[NU][NXt] = {
+  {-0.000001f,0.000009f,52653.331606f,-0.000038f,-0.000003f,0.000007f,-0.000001f,0.000006f,29655.145945f,-0.000000f,-0.000000f,0.000000f,25000.0f},
+  {4.784756f,-101.152975f,0.000000f,659.527909f,31.294875f,1.926459f,4.581155f,-96.739384f,0.000000f,57.083180f,2.717272f,1.009751f,0.0f},
+  {100.807059f,-4.790179f,-0.000000f,31.328106f,657.393357f,5.037699f,96.414917f,-4.586228f,0.000000f,2.719971f,56.909347f,2.635135f,0.0f},
+  {6.291231f,-2.509466f,-0.000000f,16.983842f,42.568022f,117.292930f,6.097435f,-2.432383f,-0.000000f,1.528248f,3.829450f,60.536232f,0.0f},
+};
+
+static float x_error[NXt] = {0};  
+static float control_input[NU] = {0};
+
+// Struct for logging position information
+static bool isInit = false;
+
+void controllerOutOfTreeInit(void) {
+  if (isInit) {
+    return;
+  }
+
+  // for (int i = 0; i < NXt; ++i) {
+  //   x_error[i] = 0.0f;
+  // }
+
+  isInit = true;
+}
+
+bool controllerOutOfTreeTest() {
+  // Always return true
+  return true;
+}
+
+void controllerOutOfTree(control_t *control, const setpoint_t *setpoint, const sensorData_t *sensors, const state_t *state, const uint32_t tick) {
+  // Control frequency
+  if (!RATE_DO_EXECUTE(LQR_RATE, tick)) {
+    return;
+  }
+  // Positon error, [m]
+  x_error[0] = state->position.x - 1*setpoint->position.x;
+  x_error[1] = state->position.y - 1*setpoint->position.y;
+  x_error[2] = state->position.z - 1*setpoint->position.z;
+
+  // Body velocity error, [m/s]                          
+  x_error[6] = state->velocity.x - 1*setpoint->velocity.x;
+  x_error[7] = state->velocity.y - 1*setpoint->velocity.y;
+  x_error[8] = state->velocity.z - 1*setpoint->velocity.z;
+
+  // Angular rate error, [rad/s]
+  x_error[9]  = radians(sensors->gyro.x - 1*setpoint->attitudeRate.roll);   
+  x_error[10] = radians(sensors->gyro.y - 1*setpoint->attitudeRate.pitch);
+  x_error[11] = radians(sensors->gyro.z - 1*setpoint->attitudeRate.yaw);
+
+  // struct quat attitude_g = qeye();  // goal attitude
+  // struct quat attitude_g = mkquat(setpoint->attitudeQuaternion.x, 
+  //                                 setpoint->attitudeQuaternion.y, 
+  //                                 setpoint->attitudeQuaternion.z, 
+  //                                 setpoint->attitudeQuaternion.w);  // do not have quat cmd
+
+  struct vec desired_rpy = mkvec(radians(setpoint->attitude.roll), 
+                                 radians(setpoint->attitude.pitch), 
+                                 radians(setpoint->attitude.yaw));
+  struct quat attitude_g = rpy2quat(desired_rpy);
+
+  struct quat attitude = mkquat(
+    state->attitudeQuaternion.x,
+    state->attitudeQuaternion.y,
+    state->attitudeQuaternion.z,
+    state->attitudeQuaternion.w);  // current attitude
+
+  struct quat attitude_gI = qinv(attitude_g);  
+  struct quat q_error = qnormalize(qqmul(attitude_gI, attitude));
+  struct vec phi = quat2rp(q_error);  // quaternion to Rodriquez parameters
+  
+  // Attitude error
+  x_error[3] = phi.x;
+  x_error[4] = phi.y;
+  x_error[5] = phi.z;
+
+  x_error[12] += x_error[2] / LQR_RATE;
+  // x_error[13] += x_error[1] / LQR_RATE;
+  // x_error[14] += x_error[2] / LQR_RATE;
+
+  // Matrix multiplication, compute control input
+  for (int i = 0; i < STABILIZER_NR_OF_MOTORS; i++){
+    control_input[i] = 0;
+
+    for (int j = 0; j < NXt; j++) {
+      control_input[i] += -K[i][j] * x_error[j];
+    }
+  }
+
+  // Compensate for gravity 
+  control_input[0] += (setpoint->thrust + 0.03f * GRAVITY_MAGNITUDE) * UINT16_MAX;
+
+  if (setpoint->mode.z == modeDisable) {
+    control->thrustSi = 0.0f;
+    control->torque[0] =  0.0f;
+    control->torque[1] =  0.0f;
+    control->torque[2] =  0.0f;
+  } else {
+    control->thrustSi = control_input[0];  // [UINT16_MAX * N]
+    control->torqueX = control_input[1];   // [N.m]
+    control->torqueY = control_input[2];
+    control->torqueZ = control_input[3];
+  } 
+  control->controlMode = controlModeForceTorqueScaled;
+  
+  // DEBUG_PRINT("%d.%.6d", (int)K[0][2], (int)((K[0][2]-(int)K[0][2])*1000000));
+  // DEBUG_PRINT("K[0][2] = %f",(double)K[0][2]);
+
+  // control->thrustSi = 0.0f;
+  // control->torque[0] =  0.0f;
+  // control->torque[1] =  0.0f;
+  // control->torque[2] =  0.0f;  
+}
+
+/**
+ * Tunning variables for the full state quaternion LQR controller
+ */
+PARAM_GROUP_START(ctrlLQR)
+/**
+ * @brief K gain
+ */
+PARAM_ADD(PARAM_FLOAT, k11, &K[0][0])
+PARAM_ADD(PARAM_FLOAT, k21, &K[1][0])
+PARAM_ADD(PARAM_FLOAT, k31, &K[2][0])
+PARAM_ADD(PARAM_FLOAT, k41, &K[3][0])
+
+PARAM_ADD(PARAM_FLOAT, k12, &K[0][1])
+PARAM_ADD(PARAM_FLOAT, k22, &K[1][1])
+PARAM_ADD(PARAM_FLOAT, k32, &K[2][1])
+PARAM_ADD(PARAM_FLOAT, k42, &K[3][1])
+
+PARAM_ADD(PARAM_FLOAT, k13, &K[0][2])
+PARAM_ADD(PARAM_FLOAT, k23, &K[1][2])
+PARAM_ADD(PARAM_FLOAT, k33, &K[2][2])
+PARAM_ADD(PARAM_FLOAT, k43, &K[3][2])
+
+PARAM_ADD(PARAM_FLOAT, k14, &K[0][3])
+PARAM_ADD(PARAM_FLOAT, k24, &K[1][3])
+PARAM_ADD(PARAM_FLOAT, k34, &K[2][3])
+PARAM_ADD(PARAM_FLOAT, k44, &K[3][3])
+
+PARAM_ADD(PARAM_FLOAT, k15, &K[0][4])
+PARAM_ADD(PARAM_FLOAT, k25, &K[1][4])
+PARAM_ADD(PARAM_FLOAT, k35, &K[2][4])
+PARAM_ADD(PARAM_FLOAT, k45, &K[3][4])
+
+PARAM_ADD(PARAM_FLOAT, k16, &K[0][5])
+PARAM_ADD(PARAM_FLOAT, k26, &K[1][5])
+PARAM_ADD(PARAM_FLOAT, k36, &K[2][5])
+PARAM_ADD(PARAM_FLOAT, k46, &K[3][5])
+
+PARAM_ADD(PARAM_FLOAT, k17, &K[0][6])
+PARAM_ADD(PARAM_FLOAT, k27, &K[1][6])
+PARAM_ADD(PARAM_FLOAT, k37, &K[2][6])
+PARAM_ADD(PARAM_FLOAT, k47, &K[3][6])
+
+PARAM_ADD(PARAM_FLOAT, k18, &K[0][7])
+PARAM_ADD(PARAM_FLOAT, k28, &K[1][7])
+PARAM_ADD(PARAM_FLOAT, k38, &K[2][7])
+PARAM_ADD(PARAM_FLOAT, k48, &K[3][7])
+
+PARAM_ADD(PARAM_FLOAT, k19, &K[0][8])
+PARAM_ADD(PARAM_FLOAT, k29, &K[1][8])
+PARAM_ADD(PARAM_FLOAT, k39, &K[2][8])
+PARAM_ADD(PARAM_FLOAT, k49, &K[3][8])
+
+PARAM_ADD(PARAM_FLOAT, k110, &K[0][9])
+PARAM_ADD(PARAM_FLOAT, k210, &K[1][9])
+PARAM_ADD(PARAM_FLOAT, k310, &K[2][9])
+PARAM_ADD(PARAM_FLOAT, k410, &K[3][9])
+
+PARAM_ADD(PARAM_FLOAT, k111, &K[0][10])
+PARAM_ADD(PARAM_FLOAT, k211, &K[1][10])
+PARAM_ADD(PARAM_FLOAT, k311, &K[2][10])
+PARAM_ADD(PARAM_FLOAT, k411, &K[3][10])
+
+PARAM_ADD(PARAM_FLOAT, k112, &K[0][11])
+PARAM_ADD(PARAM_FLOAT, k212, &K[1][11])
+PARAM_ADD(PARAM_FLOAT, k312, &K[2][11])
+PARAM_ADD(PARAM_FLOAT, k412, &K[3][11])
+
+PARAM_GROUP_STOP(ctrlLQR)
+
+/**
+ * Logging variables for the command and reference signals for the
+ * LQR controller
+ */
+
+LOG_GROUP_START(ctrlLQR)
+
+LOG_ADD(LOG_FLOAT, e_x, &x_error[0])
+LOG_ADD(LOG_FLOAT, e_y, &x_error[1])
+LOG_ADD(LOG_FLOAT, e_z, &x_error[2])
+
+LOG_ADD(LOG_FLOAT, e_roll,  &x_error[3])
+LOG_ADD(LOG_FLOAT, e_pitch, &x_error[4])
+LOG_ADD(LOG_FLOAT, e_yaw,   &x_error[5])
+
+LOG_ADD(LOG_FLOAT, e_vx, &x_error[6])
+LOG_ADD(LOG_FLOAT, e_vy, &x_error[7])
+LOG_ADD(LOG_FLOAT, e_vz, &x_error[8])
+
+LOG_ADD(LOG_FLOAT, e_vroll,  &x_error[9])
+LOG_ADD(LOG_FLOAT, e_vpitch, &x_error[10])
+LOG_ADD(LOG_FLOAT, e_vyaw,   &x_error[11])
+
+LOG_GROUP_STOP(ctrlLQR)
