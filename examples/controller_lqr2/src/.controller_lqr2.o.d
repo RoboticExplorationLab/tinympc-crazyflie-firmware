cff845c963c483cb6755e00694671a8dce287a09
Sam Schoedel <seschoedel@gmail.com>
2023-06-13 14:55:22 -0400

    up

---
+++ examples/controller_lqr2/src/.controller_lqr2.o.d
@@ -0,0 +1,49 @@
+controller_lqr2.o: \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/examples/controller_lqr2/src/controller_lqr2.c \
+ /usr/include/newlib/string.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/reent.h \
+ /usr/include/newlib/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/sys/_locale.h /usr/include/newlib/strings.h \
+ /usr/include/newlib/sys/string.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/modules/interface/app.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/vendor/FreeRTOS/include/FreeRTOS.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/config/FreeRTOSConfig.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/config/config.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/drivers/interface/nrf24l01.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/drivers/interface/nRF24L01reg.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/config/trace.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/hal/interface/usec_time.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/utils/interface/cfassert.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/vendor/FreeRTOS/include/projdefs.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/vendor/FreeRTOS/include/portable.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/vendor/FreeRTOS/include/deprecated_definitions.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/vendor/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/vendor/FreeRTOS/include/mpu_wrappers.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/vendor/FreeRTOS/include/task.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/vendor/FreeRTOS/include/list.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/modules/interface/controller/controller.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/modules/interface/stabilizer_types.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/hal/interface/imu_types.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/utils/interface/lighthouse/lighthouse_types.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/utils/interface/physicalConstants.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/modules/interface/log.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/modules/interface/param.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/modules/interface/param_logic.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/modules/interface/crtp.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/utils/interface/num.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/modules/interface/math3d.h \
+ /usr/include/newlib/math.h /usr/include/newlib/machine/fastmath.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/utils/interface/debug.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/config/config.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/modules/interface/console.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/src/utils/interface/eprintf.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h \
+ /home/rexlab/crazyflie-firmware-new/tinympc-crazyflie-firmware/examples/controller_lqr2/build/include/generated/autoconf.h
