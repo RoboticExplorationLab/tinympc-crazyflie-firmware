606847d75b37021cbe4a355a1c2f9953c4bc264b
Sam Schoedel <seschoedel@gmail.com>
2023-09-08 15:20:39 -0400

    Add new logging headers

--- tools/usdlog/test_inToOut.py
+++ tools/usdlog/test_inToOut.py
@@ -20,14 +20,13 @@ logDataFixed = logData_all['fixedFrequency']
 logData1 = logData_all['horizon_part1']
 logData2 = logData_all['horizon_part2']
 logData3 = logData_all['horizon_part3']
+# logDataXref = logData_all['xref_event']
+logDataIters = logData_all['iters_event']
+logDataCacheLevel = logData_all['cache_level_event']
 
 # set window background to white
 plt.rcParams['figure.facecolor'] = 'w'
     
-# number of columns and rows for suplot
-plotCols = 1
-plotRows = 1
-
 # # let's see which keys exists in current data set
 # keys = ""
 # for k, v in logData.items():
@@ -39,37 +38,55 @@ plotCurrent = 0
 # new figure
 plt.figure(args.filename)
 
-plt.subplot(2,1, 1)
+plt.subplot(3,1, 1)
 
-plt.plot(logData1['timestamp'], logData1['h0'], '-')#, label="z_0")
-plt.plot(logData1['timestamp'], logData1['h1'], '-')#, label="z_1")
+plt.plot(logData1['timestamp'], logData1['h0'], '-', label="z_0")
+# plt.plot(logData1['timestamp'], logData1['h1'], '--')#, label="z_1")
 plt.plot(logData1['timestamp'], logData1['h2'], '-')#, label="z_2")
-plt.plot(logData1['timestamp'], logData1['h3'], '-')#, label="z_3")
+# plt.plot(logData1['timestamp'], logData1['h3'], '--')#, label="z_3")
 plt.plot(logData1['timestamp'], logData1['h4'], '-')#, label="z_4")
 
-plt.plot(logData2['timestamp'], logData2['h5'], '-')#, label="z_5")
+# plt.plot(logData2['timestamp'], logData2['h5'], '--')#, label="z_5")
 plt.plot(logData2['timestamp'], logData2['h6'], '-')#, label="z_6")
-plt.plot(logData2['timestamp'], logData2['h7'], '-')#, label="z_7")
+# plt.plot(logData2['timestamp'], logData2['h7'], '--')#, label="z_7")
 plt.plot(logData2['timestamp'], logData2['h8'], '-')#, label="z_8")
-plt.plot(logData2['timestamp'], logData2['h9'], '-')#, label="z_9")
+# plt.plot(logData2['timestamp'], logData2['h9'], '--')#, label="z_9")
 
-plt.plot(logData3['timestamp'], logData3['h10'], '-')#, label="z_10")
+# plt.plot(logData3['timestamp'], logData3['h10'], '--')#, label="z_10")
 plt.plot(logData3['timestamp'], logData3['h11'], '-')#, label="z_11")
-plt.plot(logData3['timestamp'], logData3['h12'], '-')#, label="z_12")
-plt.plot(logData3['timestamp'], logData3['h13'], '-')#, label="z_13")
-plt.plot(logData3['timestamp'], logData3['h14'], '-')#, label="z_14")
-# plt.plot(logData['timestamp'], logData['tinympc.horizon_nh_z'], '-', label="Z_Nh")
+# plt.plot(logData3['timestamp'], logData3['h12'], '--')#, label="z_12")
+# plt.plot(logData3['timestamp'], logData3['h13'], '-')#, label="z_13")
+plt.plot(logData3['timestamp'], logData3['h14'], '-', label="z_14")
+
+# plt.plot(logDataXref['timestamp'], logDataXref['xref_z'], '-', label="xref_z")
+
+
 plt.xlabel('timestamp [ms]')
 plt.ylabel('Position [m]')
+plt.legend()
+plt.grid()
 
 
+plt.subplot(3,1, 2)
 
-plt.subplot(2,1, 2)
-
-plt.plot(logDataFixed['timestamp'], logDataFixed['tinympc.initial_velocity'], '-', label="initial z vel")
-
+plt.scatter(logDataIters['timestamp'], logDataIters['iters'], s=.5)
 plt.xlabel('timestamp [ms]')
-plt.ylabel('Velocity [m/s]')
+plt.ylabel('ADMM iters')
+plt.grid()
+
+
+plt.subplot(3,1, 3)
+
+plt.plot(logDataCacheLevel['timestamp'], logDataCacheLevel['level'], label="cache level")
+plt.xlabel('timestamp [ms]')
+plt.ylabel('Cache level')
+plt.grid()
+
+# plt.plot(logDataFixed['timestamp'], logDataFixed['tinympc.initial_velocity'], '-', label="initial z vel")
+# # plt.axhline(0, linestyle='--', color='r')
+# plt.xlabel('timestamp [ms]')
+# plt.ylabel('Velocity [m/s]')
+# plt.grid()
 
 
 
